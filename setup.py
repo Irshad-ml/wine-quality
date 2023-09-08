43bef5e20117f115ef9f0b57519355ab2b3a19ff
Md Irshad <m3irshad3@gmail.com>
2023-09-08 07:26:09 +0530

    requirements.txt created and setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='Md Irshad',
+    author_email='m3irshad3@gmail.com',
+    description='A small python package for ml app',
+    packages=find_packages()
+)
